80974238d98c20ce22db220eb620c095db2a2c8b
Hüseyin <74729922+huseyin07@users.noreply.github.com>
2026-08-17 21:21:19 +0300

    feat: compose TILE homepage enhancements

---
+++ components/EnhancedPage.tsx
@@ -0,0 +1,4 @@
+import {WhitelistJourney} from "@/components/WhitelistJourney";
+import SiteEnhancements2 from "@/components/SiteEnhancements2";
+
+export default function EnhancedPage(){return <><WhitelistJourney/><SiteEnhancements2/></>}
