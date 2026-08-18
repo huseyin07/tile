6ae0ff8abbac56cad3b265642fa2020cc2b9603a
Hüseyin <74729922+huseyin07@users.noreply.github.com>
2026-08-18 10:44:15 +0300

    fix: mount whitelist UX fixes

--- components/EnhancedPage.tsx
+++ components/EnhancedPage.tsx
@@ -1,4 +1,5 @@
 import {WhitelistJourney} from "@/components/WhitelistJourney";
 import SiteEnhancements2 from "@/components/SiteEnhancements2";
+import WhitelistUxFixes from "@/components/WhitelistUxFixes";
 
-export default function EnhancedPage(){return <><WhitelistJourney/><SiteEnhancements2/></>}
+export default function EnhancedPage(){return <><WhitelistJourney/><WhitelistUxFixes/><SiteEnhancements2/></>}
