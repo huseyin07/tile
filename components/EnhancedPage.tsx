42bbd6cc3ebd9effe946bf1d660ae6ec10f35b4d
Hüseyin <74729922+huseyin07@users.noreply.github.com>
2026-08-20 19:53:23 +0300

    feat: mount TILE collection preview after About

--- components/EnhancedPage.tsx
+++ components/EnhancedPage.tsx
@@ -1,5 +1,6 @@
 import {WhitelistJourney} from "@/components/WhitelistJourney";
 import SiteEnhancements2 from "@/components/SiteEnhancements2";
 import WhitelistUxFixes from "@/components/WhitelistUxFixes";
+import CollectionPreview from "@/components/CollectionPreview";
 
-export default function EnhancedPage(){return <><WhitelistJourney/><WhitelistUxFixes/><SiteEnhancements2/></>}
+export default function EnhancedPage(){return <><WhitelistJourney/><CollectionPreview/><WhitelistUxFixes/><SiteEnhancements2/></>}
