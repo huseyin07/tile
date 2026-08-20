d7855f9774baeb49178f5590fa2b7d3bacf51bb8
Hüseyin <74729922+huseyin07@users.noreply.github.com>
2026-08-20 22:29:23 +0300

    remove NFT collection preview section

--- components/EnhancedPage.tsx
+++ components/EnhancedPage.tsx
@@ -2,6 +2,5 @@ import {WhitelistJourney} from "@/components/WhitelistJourney";
 import SiteEnhancements2 from "@/components/SiteEnhancements2";
 import WhitelistUxFixes from "@/components/WhitelistUxFixes";
 import SitePolish from "@/components/SitePolish";
-import CollectionPreview from "@/components/CollectionPreview";
 
-export default function EnhancedPage(){return <><WhitelistJourney/><CollectionPreview/><WhitelistUxFixes/><SiteEnhancements2/><SitePolish/></>}
+export default function EnhancedPage(){return <><WhitelistJourney/><WhitelistUxFixes/><SiteEnhancements2/><SitePolish/></>}
