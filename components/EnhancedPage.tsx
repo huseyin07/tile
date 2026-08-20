f6d8ecc3b838f2ae122066678403007af468b88e
Hüseyin <74729922+huseyin07@users.noreply.github.com>
2026-08-20 20:07:13 +0300

    feat: enable premium site polish

--- components/EnhancedPage.tsx
+++ components/EnhancedPage.tsx
@@ -1,5 +1,6 @@
 import {WhitelistJourney} from "@/components/WhitelistJourney";
 import SiteEnhancements2 from "@/components/SiteEnhancements2";
 import WhitelistUxFixes from "@/components/WhitelistUxFixes";
+import SitePolish from "@/components/SitePolish";
 
-export default function EnhancedPage(){return <><WhitelistJourney/><WhitelistUxFixes/><SiteEnhancements2/></>}
+export default function EnhancedPage(){return <><WhitelistJourney/><WhitelistUxFixes/><SiteEnhancements2/><SitePolish/></>}
