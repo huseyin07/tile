69572a9aa90e055e281e98068df0f1526d53df25
Hüseyin <74729922+huseyin07@users.noreply.github.com>
2026-08-21 15:13:51 +0300

    feat: enable TILE ambient Korean music

--- components/EnhancedPage.tsx
+++ components/EnhancedPage.tsx
@@ -2,5 +2,6 @@ import {WhitelistJourney} from "@/components/WhitelistJourney";
 import SiteEnhancements2 from "@/components/SiteEnhancements2";
 import WhitelistUxFixes from "@/components/WhitelistUxFixes";
 import SitePolish from "@/components/SitePolish";
+import AmbientMusic from "@/components/AmbientMusic";
 
-export default function EnhancedPage(){return <><WhitelistJourney/><WhitelistUxFixes/><SiteEnhancements2/><SitePolish/></>}
+export default function EnhancedPage(){return <><WhitelistJourney/><WhitelistUxFixes/><SiteEnhancements2/><SitePolish/><AmbientMusic/></>}
